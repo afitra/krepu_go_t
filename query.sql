95862deda2d0ece113cdbdea0d57215e173c3bbc
afitra <afitra.vendor.pasti@pegadaian.co.id>
2023-12-24 01:26:21 +0700

    add docker config

--- query.sql
+++ query.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     nik VARCHAR(255) NOT NULL UNIQUE,
     user_name VARCHAR(255) NOT NULL UNIQUE,
@@ -15,7 +15,7 @@ CREATE TABLE users (
 
 
 
-CREATE TABLE transactions (
+CREATE TABLE IF NOT EXISTS transactions (
     id SERIAL PRIMARY KEY NOT NULL,
     user_id integer REFERENCES users(id) NOT NULL,
     no_kontrak character varying(255) NOT NULL,
@@ -26,3 +26,12 @@ CREATE TABLE transactions (
     nama_asset character varying(255) NOT NULL,
     status boolean NOT NULL DEFAULT false
 );
+<<<<<<< Updated upstream
+=======
+
+
+
+INSERT INTO "users" ( "nik", "user_name", "password", "full_name", "legal_name", "tempat_lahir", "tanggal_lahir", "gaji", "foto_ktp", "foto_selfie", "role", "tenor_satu", "tenor_dua", "tenor_tiga", "tenor_empat") VALUES
+('3493493049d3049',	'apitoong',	'$2a$10$pOJ7JS/Uqqk68VZDi0ZimeGwDpxAmZ8s5oo0H2mnG6fhT4rAD/N/u',	'Afitra Mamor Bikhoir',	'Afitra Mamor Bikhoir',	'kediri',	'03-03-1996',	10000000,	'https://img.inews.co.id/media/600/files/networks/2022/06/20/ffb4c_anya-geraldine.jpg',	'https://img.inews.co.id/media/600/files/networks/2022/06/20/ffb4c_anya-geraldine.jpg',	'user',	1200000,	1800000,	2000000,	2000000),
+('34934930493049',	'admin123',	'$2a$10$Efh/oCrUYazpvci7Gh5P7Or/zA7vkeMuQfQRyv0ABFlrMaPhBx2GW',	'Afitra Mamor Bikhoir',	'Afitra Mamor Bikhoir',	'kediri',	'03-03-1996',	10000000,	'https://img.inews.co.id/media/600/files/networks/2022/06/20/ffb4c_anya-geraldine.jpg',	'https://img.inews.co.id/media/600/files/networks/2022/06/20/ffb4c_anya-geraldine.jpg',	'admin',	600000,	1000000,	1800000,	1800000);
+>>>>>>> Stashed changes
